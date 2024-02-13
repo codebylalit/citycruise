e5b590cedce0ec92d9e133644c0ccf4ba7fefdae
codebylalit <lalitkumarnamdev4645@gmail.com>
2024-02-13 14:18:34 +0530

    city

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [ // Corrected property name here
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path : ".env",
+        },
+      ],
+    ],
   };
 };
